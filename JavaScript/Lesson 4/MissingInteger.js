28c3b24098b0c6cbe4776d5971b6f3792333d339
kunzimariano <kunzi.mariano@gmail.com>
2017-02-16 20:55:22 -0300

    A cleaner MissingInteger.

--- JavaScript/Lesson 4/MissingInteger.js
+++ JavaScript/Lesson 4/MissingInteger.js
@@ -1,24 +1,40 @@
 'use strict';
 
-let getNextMin = function(number, done) {
-  let n = number;
-  while (true) {
-    n++;
-    if(done[n] === undefined) {
-      return n;
-    }
-  }
-};
-
 let solution = function(A) {
-  let min = 1;
-  let done = [];
+  const numbers = {};
+  let start = Number.MAX_SAFE_INTEGER;
+  let end = Number.MIN_SAFE_INTEGER;
 
-  for (let i = 0; i < A.length; i++) {
-    done[A[i]] = 1;
-    if (A[i] == min) {
-      min = getNextMin(min, done);
+  A.forEach(item => {
+    numbers[item] = true;
+    if (item < start) { start = item; }
+    if (item > end) { end = item; }
+  });
+
+  if (end <= 0) {
+    return 1;
+  }
+
+  start = 1;
+  let result = end + 1;
+
+  for (let i = start; i < end; i++) {
+    if (!numbers[i]) {
+      result = i;
+      break;
     }
   }
-  return min;
+
+  return result;
 };
+
+const log = console.log;
+
+let r = solution([-10, 5, 9, 3, -9, -7, -8]);
+log(r);
+
+r = solution([2]);
+log(r);
+
+r = solution([-1,0,1,2,5]);
+log(r);
